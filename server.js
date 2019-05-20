b7905f81ac903c0b91085775c4b23ad10fd1bb26
David Zukor <zukord@gmail.com>
2019-05-20 01:34:12 -0400

    Working with NYTimes

--- server.js
+++ server.js
@@ -41,25 +41,25 @@ mongoose.connect(MONGODB_URI);
 // A GET route for scraping the echoJS website
 app.get("/scrape", function(req, res) {
   // First, we grab the body of the html with axios
-  axios.get("https://arstechnica.com/").then(function(response) {
+  axios.get("https://www.nytimes.com").then(function(response) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(response.data);
 
     // Now, we grab every h2 within an article tag, and do the following:
-    $("li.").each(function(i, element) {
+    $("div.css-1qiat4j").each(function(i, element) {
       // Save an empty result object
       var result = {};
 
       // Add the text and href of every link, and save them as properties of the result object
       result.title = $(this)
-        .children("a")
+      .find("span")
         .text();
       result.link = $(this)
-        .children("a")
+      .find("a")
         .attr("href");
-      result.summary = $(this)
-        .children("p.exerpt")
-        .text();
+/*       result.summary = $(this)
+        .children("p.bQeEFC")
+        .text(); */
       
         console.log(result.title);
 
